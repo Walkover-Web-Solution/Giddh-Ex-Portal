802aadff444c78ef9529c1190e3fdf81eb9cf6ea
Dilpreet Singh Dang <dilpreet@whozzat.com>
2023-12-05 12:44:08 +0530

    fixed issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { ReplaySubject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+    public sidebarState: boolean;
+    /** Observable to unsubscribe all the store listeners to avoid memory leaks */
+    private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
 
-    constructor() {
+    constructor(private store: Store) {
+    }
+
+    /**
+     *  This will be use for component initialization
+     *
+     * @memberof AppComponent
+     */
+    public ngOnInit(): void {
+        this.store.pipe(select(state => state), takeUntil(this.destroyed$)).subscribe((sessionState: any) => {
+            if (sessionState.session) {
+                this.sidebarState = sessionState.session.sidebarState;
+            }
+        });
+
     }
 }
