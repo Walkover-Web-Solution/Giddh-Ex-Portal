550a1acd6f33a2d37f31146c29362d2373c20167
ravindersingh <ravinder@walkover.in>
2023-12-25 19:20:05 +0530

    fixed giddh api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
     production: false,
     apiUrl: "https://routes.msg91.com/api/proxy/117230/24lvqun1/",
     proxyUrl: "https://routes.msg91.com/",
-    proxyReferenceId: "117230p1697093599652797df30cea"
+    proxyReferenceId: "117230p1697093599652797df30cea",
+    giddhApiUrl: "https://apitest.giddh.com/"
 };
 
 /*
