bc155b5db7ee0726645a2d08c3b148ee7e025b50
Dilpreet Singh Dang <dilpreet@whozzat.com>
2025-01-20 18:41:27 +0530

    PR issue

--- src/app/invoice-preview/invoice-preview.component.ts
+++ src/app/invoice-preview/invoice-preview.component.ts
@@ -95,7 +95,7 @@ export class InvoicePreviewComponent implements OnInit, OnDestroy {
                 this.queryParams = response[0];
                 this.urlParams = response[1];
                 this.storeData = response[2]['folderName'][this.urlParams?.companyDomainUniqueName];
-                this.region = this.storeData?.region ?? response[1]?.region ;
+                this.region = this.storeData?.region ?? response[1]?.region;
                 if (!this.storeData?.session?.id) {
                     this.storeData = {
                         session: {
@@ -159,7 +159,7 @@ export class InvoicePreviewComponent implements OnInit, OnDestroy {
         this.isLoading = true;
         const accountUniqueName = this.storeData.userDetails?.account.uniqueName ?? this.queryParams?.accountUniqueName;
         const companyUniqueName = this.storeData.userDetails?.companyUniqueName ?? this.queryParams.companyUniqueName;
-        const request = { accountUniqueName: accountUniqueName, companyUniqueName: companyUniqueName, sessionId: this.storeData.session?.id };
+        const request = { accountUniqueName, companyUniqueName, sessionId: this.storeData.session?.id };
         this.invoiceService.getPaymentMethods(request).pipe(takeUntil(this.destroyed$)).subscribe(response => {
             this.isLoading = false;
             if (response && response.status === 'success') {
