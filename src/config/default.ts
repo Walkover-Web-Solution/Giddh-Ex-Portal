1ab18a8ec7961fd2d4f46abb64d92293be8fe41d
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2026-01-29 16:48:46 +0530

    Updated default configuration reference ID and reorganized config properties for better readability

--- src/config/default.ts
+++ src/config/default.ts
@@ -11,11 +11,13 @@ export interface AppConfig {
 
 export const DEFAULT_CONFIG: AppConfig = {
   disableWhiteLabel: true,
-  NEXT_PUBLIC_REFERENCE_ID: "1362783l1767680954695cabba5ada1",
-  NEXT_PUBLIC_GIDDH_API_URL: "https://apitest.giddh.com",
-  NEXT_PUBLIC_PROXY_URL: "https://routes.msg91.com",
+  NEXT_PUBLIC_REFERENCE_ID: "117230p1697093599652797df30cea",
   NEXT_PUBLIC_API_URL: "https://routes.msg91.com/api/proxy/117230/24lvqun1",
-  NEXT_PUBLIC_PAYPAL_URL: "https://www.sandbox.paypal.com/cgi-bin/webscr",
+
   NEXT_PUBLIC_REFERENCE_ID_UK: "117230d172709659666f16714325b0",
   NEXT_PUBLIC_API_URL_UK: "https://routes.msg91.com/api/proxy/117230/34ytsup2",
+
+  NEXT_PUBLIC_GIDDH_API_URL: "https://apitest.giddh.com",
+  NEXT_PUBLIC_PROXY_URL: "https://routes.msg91.com",
+  NEXT_PUBLIC_PAYPAL_URL: "https://www.sandbox.paypal.com/cgi-bin/webscr",
 };
