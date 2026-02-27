ee79baf2957b689b7a63c0db1164ea57040742fa
Giddh's Black Tiger <divyanshushrivastava123@gmail.com>
2026-02-27 11:56:24 +0530

    refactor: increase API client timeout from 30s to 60s

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -8,7 +8,7 @@ class ApiClient {
 
   constructor() {
     this.instance = axios.create({
-      timeout: 30000,
+      timeout: 60000,
       headers: {
         "Content-Type": "application/json",
       },
